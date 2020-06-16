353e41d67e339c0e20562b307e83dff2ac51e9f7
Noémien Kocher <nkcr.je@gmail.com>
2020-06-16 22:04:09 +0200

    Adds a contact page

---
+++ bullenetwork.ch/src/pages/contact.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react"
+import Layout from "../components/layout"
+import Intro from "../components/intro"
+
+export default () => {
+  const [title, setTitle] = useState(0)
+  const [content, setContent] = useState(1)
+  useEffect(() => {
+    setTitle(
+      <div className="loadrr"><div><div></div><div></div><div></div><div></div></div></div>
+    )
+    setContent(
+      '<div class="loadrr"><div><div></div><div></div><div></div><div></div></div></div>'
+    )
+    fetch(`https://panda.bullenetwork.ch/directus/items/bullenetwork_pages/4`)
+      .then(response => response.json())
+      .then(resultData => {
+        setTitle(resultData.data.title)
+        setContent(resultData.data.content)
+      })
+  }, [])
+  return (
+    <Layout layoutClass="index-container">
+      <Intro>
+        {title}
+      </Intro>
+      <section id="text-info">
+        <div dangerouslySetInnerHTML={{ __html: content }} className="section-container" />
+      </section>
+    </Layout>
+  )
+}
