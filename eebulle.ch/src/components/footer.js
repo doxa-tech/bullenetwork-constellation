94f23096eafa1e736d7828eb402f8ebf46e4ba08
Noémien Kocher <nkcr.je@gmail.com>
2020-07-02 23:21:04 +0200

    Adds responsive design on the home page

--- eebulle.ch/src/components/footer.js
+++ eebulle.ch/src/components/footer.js
@@ -38,14 +38,14 @@ const Footer = () => {
             <p>Route du Verdel 8</p>
             <p>1630 Bulle</p>
             <p>Bâtiment Riedo, 1er étage</p>
-            <br/>
+            <br />
             <p>CCP 17-559407-8 </p>
             <a>En savoir plus sur nos finances</a>
           </div>
 
-          <div className="column">
-            <h4>Nos réseaux</h4>
-            <div className="networks">
+          <div className="column networks">
+            <div class="wrapper">
+              <h4>Nos réseaux</h4>
               <a href="https://www.youtube.com/channel/UCL_B5hW5uxe-0P8kPLma9gg" target="_blank" rel="noreferrer">
                 <Img fixed={data.youtube.childImageSharp.fixed} />
               </a>
@@ -58,17 +58,22 @@ const Footer = () => {
             </div>
           </div>
 
-          <div className="column">
-            <h4><a href="https://bullenetwork.ch" target="_blank" rel="noreferrer">Le Bulle Network</a></h4>
-            <p><a href="https://eebullee.ch" target="_blank" rel="noreferrer">Bulle</a></p>
-            <p><a href="https://eglisesurleroc.ch" target="_blank" rel="noreferrer">Fribourg</a></p>
-            <p><a href="https://eeromont.ch" target="_blank" rel="noreferrer">Romont</a></p>
-            <p><a href="https://enhaut.eebulle.ch" target="_blank" rel="noreferrer">Pays d'Enhaut</a></p>
-            <p><a href="http://www.impact-stecroix.ch" target="_blank" rel="noreferrer">Impact Ste-Croix</a></p>
+          <div className="column bn-network">
+            <div class="wrapper">
+              <h4><a href="https://bullenetwork.ch" target="_blank" rel="noreferrer">Le Bulle Network</a></h4>
+              <p><a href="https://eebullee.ch" target="_blank" rel="noreferrer">Bulle</a></p>
+              <p><a href="https://eglisesurleroc.ch" target="_blank" rel="noreferrer">Fribourg</a></p>
+              <p><a href="https://eeromont.ch" target="_blank" rel="noreferrer">Romont</a></p>
+              <p><a href="https://enhaut.eebulle.ch" target="_blank" rel="noreferrer">Pays d'Enhaut</a></p>
+              <p><a href="http://www.impact-stecroix.ch" target="_blank" rel="noreferrer">Impact Ste-Croix</a></p>
+            </div>
           </div>
         </div>
 
-        <p className="copyright">© EEBulle {new Date().getFullYear()}</p>
+        <p className="copyright">
+          © EEBulle {new Date().getFullYear()}<br />
+          <a href="https://github.com/doxa-tech/bullenetwork-constellation">Github</a>
+        </p>
       </div>
     </footer>
   )
