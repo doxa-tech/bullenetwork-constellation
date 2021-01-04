339bf8d4aca6f7318f374ebe969e3f63c8c81906
Noémien Kocher <nkcr.je@gmail.com>
2021-01-04 20:36:49 +0100

    Removes the special section

--- bullenetwork.ch/src/pages/index.js
+++ bullenetwork.ch/src/pages/index.js
@@ -7,7 +7,7 @@ export default () => {
   const [title, setTitle] = useState(0)
   const [content, setContent] = useState(1)
   const [churches, setChurches] = useState(2)
-  const [specialContent, setSpecialContent] = useState(3)
+  // const [specialContent, setSpecialContent] = useState(3)
   useEffect(() => {
     setTitle(
       <div className="loadrr"><div><div></div><div></div><div></div><div></div></div></div>
@@ -15,20 +15,20 @@ export default () => {
     setContent(
       '<div class="loadrr"><div><div></div><div></div><div></div><div></div></div></div>'
     )
-    setSpecialContent(
-      '<div class="loadrr"><div><div></div><div></div><div></div><div></div></div></div>'
-    )
+    // setSpecialContent(
+    //   '<div class="loadrr"><div><div></div><div></div><div></div><div></div></div></div>'
+    // )
     fetch(`https://panda.bullenetwork.ch/directus/items/bullenetwork_pages/2`)
       .then(response => response.json())
       .then(resultData => {
         setTitle(resultData.data.title)
         setContent(resultData.data.content)
       })
-    fetch(`https://panda.bullenetwork.ch/directus/items/bullenetwork_pages/3`)
-      .then(response => response.json())
-      .then(resultData => {
-        setSpecialContent(resultData.data.content)
-      })
+    // fetch(`https://panda.bullenetwork.ch/directus/items/bullenetwork_pages/3`)
+    //   .then(response => response.json())
+    //   .then(resultData => {
+    //     setSpecialContent(resultData.data.content)
+    //   })
     fetch(`https://panda.bullenetwork.ch/directus/items/bullenetwork_churches`)
       .then(response => response.json())
       .then(resultData => {
@@ -50,12 +50,12 @@ export default () => {
         {title}
       </Intro>
 
-      <section id="text-info">
+      {/* <section id="text-info">
 
         <div dangerouslySetInnerHTML={{ __html: specialContent }} className="section-container" style={{ border: "2px solid orange", background: "rgb(255, 243, 220)", margin: "30px 0 0 0", padding: "30px" }}>
         </div>
 
-      </section>
+      </section> */}
 
       <section id="churches">
         <div className="section-container">
