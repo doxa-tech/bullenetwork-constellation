4ffd1f28629e4000210510a65c0aa1360e9e6a1b
Noémien Kocher <nkcr.je@gmail.com>
2022-02-06 23:25:53 +0100

    Adds 404 page on eebulle.ch

--- eebulle.ch/src/pages/404.js
+++ eebulle.ch/src/pages/404.js
@@ -3,11 +3,15 @@ import React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
+import "./404.scss"
+
 const NotFoundPage = () => (
-  <Layout>
+  <Layout layoutClass="error-404">
     <Seo title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn't exist... the sadness.</p>
+    <div className="container">
+      <h1>PAGE INTROUVABLE</h1>
+      <p>La page recherchée n'existe pas. (erreur 404)</p>
+    </div>
   </Layout>
 )
 
