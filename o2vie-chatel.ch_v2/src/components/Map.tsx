de79245cd95c6dcf3d1c1462cc2389c2439e2c00
Noémien Kocher <nkcr.je@gmail.com>
2023-05-13 23:46:23 +0200

    Fixes map icons on O2Vie

--- o2vie-chatel.ch_v2/src/components/Map.tsx
+++ o2vie-chatel.ch_v2/src/components/Map.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react"
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
+import { icon } from "leaflet"
 import CSS from "./Map.module.scss"
 
 import "./map.scss"
@@ -8,6 +9,15 @@ import "./leaflet.scss"
 const Markers = () => {
   const [markers, setMarkers] = useState(null)
 
+  const markerIcon = icon({
+    iconSize: [25, 41],
+    iconAnchor: [10, 41],
+    popupAnchor: [2, -40],
+    iconUrl: "leaflet/marker-icon-2x.png",
+    iconRetinaUrl: "leaflet/marker-icon-2x.png",
+    shadowUrl: "leaflet/marker-shadow.png",
+  });
+
   useEffect(() => {
     fetch(`${import.meta.env.PUBLIC_DIRECTUS_ENDPOINT}/items/o2vie_locations`)
       .then(response => response.json())
@@ -15,7 +25,7 @@ const Markers = () => {
         var locations = json.data
 
         setMarkers(locations.map(g =>
-          <Marker key={g.id} position={[g.location.coordinates[1], g.location.coordinates[0]]}>
+          <Marker icon={markerIcon} key={g.id} position={[g.location.coordinates[1], g.location.coordinates[0]]}>
             <Popup>{g.description}</Popup>
           </Marker>
         ))
